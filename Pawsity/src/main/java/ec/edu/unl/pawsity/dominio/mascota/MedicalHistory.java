f922ae0d2ba487b2092c997aed3ad91fd1cca383
Julián Merino <julianp.mg2008@gmail.com>
2026-05-25 15:41:03 -0500

    implementacion de clases para los parametros mascota

---
+++ Pawsity/src/main/java/ec/edu/unl/pawsity/dominio/mascota/MedicalHistory.java
@@ -0,0 +1,36 @@
+package ec.edu.unl.pawsity.dominio.mascota;
+
+import java.util.Date;
+
+public class MedicalHistory {
+
+    private Date fechaCreacion;
+    private String ultimaVacuna;
+    private String tratamientoActual;
+    private String observacion;
+
+    public MedicalHistory(Date fechaCreacion, String ultimaVacuna, String tratamientoActual, String observacion) {
+        this.fechaCreacion = fechaCreacion;
+        this.ultimaVacuna = ultimaVacuna;
+        this.tratamientoActual = tratamientoActual;
+        this.observacion = observacion;
+    }
+
+    public void actualizarHistorial() {
+        // TODO: Implementar la lógica para actualizar el historial
+        System.out.println("Historial médico actualizado.");
+    }
+
+    // Getters y Setters
+    public Date getFechaCreacion() { return fechaCreacion; }
+    public void setFechaCreacion(Date fechaCreacion) { this.fechaCreacion = fechaCreacion; }
+
+    public String getUltimaVacuna() { return ultimaVacuna; }
+    public void setUltimaVacuna(String ultimaVacuna) { this.ultimaVacuna = ultimaVacuna; }
+
+    public String getTratamientoActual() { return tratamientoActual; }
+    public void setTratamientoActual(String tratamientoActual) { this.tratamientoActual = tratamientoActual; }
+
+    public String getObservacion() { return observacion; }
+    public void setObservacion(String observacion) { this.observacion = observacion; }
+}
---
+++ Pawsity/src/main/java/ec/edu/unl/pawsity/dominio/mascota/StatePet.java
@@ -0,0 +1,8 @@
+package ec.edu.unl.pawsity.dominio.mascota;
+
+public enum StatePet {
+    DISPONIBLE,
+    EN_PROCESO,
+    ADOPTADO
+}
+
